74478f88f9c61b4a1b67996ac042b534dece3b81
jberends <jberends@jbits.nl>
2012-10-02 22:01:48 +0200

    updated travis integration file

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ __author__ = 'Jochem Berends'
 __author_email__ = 'jberends+xppkg@jbits.nl'
 
 from xppkg import __version__
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import sys
 
 classifiers = """\
 Development Status :: 2 - Pre-Alpha
@@ -17,6 +21,7 @@ Programming Language :: Python
 Topic :: Games/Entertainment :: Simulation
 """
 
+tests_require = ['nose',]
 
 setup(
     name='XPpkg',
@@ -25,8 +30,25 @@ setup(
     url='https://github.com/jberends/xppkg',
     version=__version__,
     description='X-Plane hassle free package installer',
+    keywords='X-Plane package installer hassle-free',
     classifiers=classifiers.splitlines(),
     packages=['xppkg',],
+
+    # These are the console scripts which are generated 'xppkg' and 'xppkg-<pyversion>'
+    # They will be in your /Scripts directory (win) or bin dir (macosx, linux)
+    entry_points={
+        'console_scripts':[
+            'xppkg=xppkg:main',
+            'xppkg-%s=xppkg:main' % sys.version[:3]
+            ],
+        },
+
     license='Attribution-NonCommercial-ShareAlike 3.0 (CC BY-NC-SA 3.0)',
     long_description=open('README.rst').read(),
+
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    extras_require = {
+        'testing':tests_require,
+        },
 )
--- xppkg/__init__.py
+++ xppkg/__init__.py
@@ -1,4 +1,31 @@
-__author__ = 'Jochem Berends'
+__author__ = 'Jochem Berends <jberends+xppkg@jbits.nl>'
+__home_page__ = 'https://github.com/jberends/xppkg'
 
 # This version is used by the setup.py
-__version__ = '0.1dev.7'
+__version__ = '0.2dev.0'
+
+import sys
+
+def main():
+    """
+    This is the main start point (entry point) of the script
+    """
+    sys.stdout.write("""
+    Thanks for using XPpkg, the X-Plane hassle free package manager.
+
+    Please remember that this is only version: %s.
+    So go check out %s
+    on which contributions are always welcome!
+
+    Regards,
+    %s
+    """ % (__version__, __home_page__, __author__))
+    pass
+
+
+
+
+if __name__ == "__main__":
+    exit = main()
+    if exit:
+        sys.exit(exit)
